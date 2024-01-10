a5de7959b4a79b8f3a36336f42961c42f3c77140
Misha <mr.kosheef54@gmail.com>
2024-01-10 17:05:02 +0500

    implement swagger; add part of swagger documentation

--- internal/rest/handler.go
+++ internal/rest/handler.go
@@ -4,12 +4,13 @@ import (
 	"database/sql"
 	"encoding/json"
 	"errors"
-	"fmt"
-	"io"
 	"log/slog"
 	"net/http"
 
+	_ "github.com/M-Koscheev/urfu-project-smart-schedule-former/docs"
 	"github.com/M-Koscheev/urfu-project-smart-schedule-former/internal/app"
+	"github.com/M-Koscheev/urfu-project-smart-schedule-former/internal/model"
+
 	"github.com/julienschmidt/httprouter"
 	uuid "github.com/satori/go.uuid"
 )
@@ -24,26 +25,41 @@ type Handler struct {
 func New(app *app.App) *Handler {
 	router := httprouter.New()
 	h := &Handler{App: app, Router: router}
-	router.GET("/api/knowledge/v1/:id", h.GetKnowledge)
-	router.GET("/api/technology/v1/:id", h.GetTechnology)
-	router.GET("/api/competency/v1/:id", h.GetCompetency)
-	router.GET("/api/profession/v1/:id", h.GetProfession)
-	router.GET("/api/project/v1/:id", h.GetProject)
-	router.GET("/api/organization/v1/:id", h.GetOrganization)
-	router.GET("/api/educationalProgram/v1/:id", h.GetEducationalProgram)
-	router.GET("/api/discipline/v1/:id", h.GetDiscipline)
-	router.GET("/api/course/v1/:id", h.GetCourse)
-	router.GET("/api/portfolio/v1/:id", h.GetPortfolio)
-	router.GET("/api/student/v1/:id", h.GetStudent)
-	router.GET("/api/trajectory/v1/:id", h.GetTrajectory)
+	router.ServeFiles("/docs/*filepath", http.Dir("docs"))
 
-	router.POST("/api/knowledge/v1/", h.PostKnowledge)
-	router.POST("/api/technology/v1/", h.PostTechnology)
+	router.GET("/api/v1/knowledge/:id", h.GetKnowledge)
+	router.GET("/api/v1/technology/:id", h.GetTechnology)
+	router.GET("/api/v1/competency/:id", h.GetCompetency)
+	router.GET("/api/v1/profession/:id", h.GetProfession)
+	router.GET("/api/v1/project/:id", h.GetProject)
+	router.GET("/api/v1/organization/:id", h.GetOrganization)
+	router.GET("/api/v1/educationalProgram/:id", h.GetEducationalProgram)
+	router.GET("/api/v1/discipline/:id", h.GetDiscipline)
+	router.GET("/api/v1/course/:id", h.GetCourse)
+	router.GET("/api/v1/portfolio/:id", h.GetPortfolio)
+	router.GET("/api/v1/student/:id", h.GetStudent)
+	router.GET("/api/v1/trajectory/:id", h.GetTrajectory)
+
+	router.POST("/api/v1/knowledge/", h.PostKnowledge)
+	router.POST("/api/v1/technology/", h.PostTechnology)
 	// router.POST("/api/competencies/v1/", h.PostCompetency)
 
 	return h
 }
 
+// GetKnowledge return knowledge by it`s id
+//
+// @Summary      Show knowledge
+// @Description  get single knowledge by ID
+// @Tags         knowledge
+// @Accept       json
+// @Produce      json
+// @Param        id   path      string  true  "Knowledge ID"
+// @Success      200  {object}  model.GetKnowledgeResponse
+// @Failure      400
+// @Failure      404
+// @Failure      500
+// @Router       /api/v1/knowledge/{id} [get]
 func (h *Handler) GetKnowledge(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
 	id, err := uuid.FromString(params.ByName("id"))
 	if err != nil {
@@ -73,6 +89,19 @@ func (h *Handler) GetKnowledge(w http.ResponseWriter, r *http.Request, params ht
 	w.Write(respJSON)
 }
 
+// GetTechnology return technology by it`s id
+//
+// @Summary      Show technology
+// @Description  get single technology by ID
+// @Tags         technology
+// @Accept       json
+// @Produce      json
+// @Param        id   path      string  true  "Technology ID"
+// @Success      200  {object}  model.GetTechnologyResponse
+// @Failure      400
+// @Failure      404
+// @Failure      500
+// @Router       /api/v1/technology/{id} [get]
 func (h *Handler) GetTechnology(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
 	id, err := uuid.FromString(params.ByName("id"))
 	if err != nil {
@@ -102,6 +131,19 @@ func (h *Handler) GetTechnology(w http.ResponseWriter, r *http.Request, params h
 	w.Write(respJSON)
 }
 
+// GetCompetency return competency by it`s id
+//
+// @Summary      Show competency
+// @Description  get single competency by ID
+// @Tags         competency
+// @Accept       json
+// @Produce      json
+// @Param        id   path      string  true  "Competency ID"
+// @Success      200  {object}  model.GetCompetencyResponse
+// @Failure      400
+// @Failure      404
+// @Failure      500
+// @Router       /api/v1/competency/{id} [get]
 func (h *Handler) GetCompetency(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
 	id, err := uuid.FromString(params.ByName("id"))
 	if err != nil {
@@ -131,6 +173,19 @@ func (h *Handler) GetCompetency(w http.ResponseWriter, r *http.Request, params h
 	w.Write(respJSON)
 }
 
+// GetProfession return profession by it`s id
+//
+// @Summary      Show profession
+// @Description  get single profession by ID
+// @Tags         profession
+// @Accept       json
+// @Produce      json
+// @Param        id   path      string  true  "Profession ID"
+// @Success      200  {object}  model.GetProfessionResponse
+// @Failure      400
+// @Failure      404
+// @Failure      500
+// @Router       /api/v1/profession/{id} [get]
 func (h *Handler) GetProfession(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
 	id, err := uuid.FromString(params.ByName("id"))
 	if err != nil {
@@ -160,6 +215,19 @@ func (h *Handler) GetProfession(w http.ResponseWriter, r *http.Request, params h
 	w.Write(respJSON)
 }
 
+// GetProject return project by it`s id
+//
+// @Summary      Show project
+// @Description  get single project by ID
+// @Tags         project
+// @Accept       json
+// @Produce      json
+// @Param        id   path      string  true  "Project ID"
+// @Success      200  {object}  model.GetProjectResponse
+// @Failure      400
+// @Failure      404
+// @Failure      500
+// @Router       /api/v1/project/{id} [get]
 func (h *Handler) GetProject(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
 	id, err := uuid.FromString(params.ByName("id"))
 	if err != nil {
@@ -189,6 +257,19 @@ func (h *Handler) GetProject(w http.ResponseWriter, r *http.Request, params http
 	w.Write(respJSON)
 }
 
+// GetOrganization return organization by it`s id
+//
+// @Summary      Show organization
+// @Description  get single organization by ID
+// @Tags         project
+// @Accept       json
+// @Produce      json
+// @Param        id   path      string  true  "Organization ID"
+// @Success      200  {object}  model.GetOrganizationResponse
+// @Failure      400
+// @Failure      404
+// @Failure      500
+// @Router       /api/v1/organization/{id} [get]
 func (h *Handler) GetOrganization(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
 	id, err := uuid.FromString(params.ByName("id"))
 	if err != nil {
@@ -218,6 +299,19 @@ func (h *Handler) GetOrganization(w http.ResponseWriter, r *http.Request, params
 	w.Write(respJSON)
 }
 
+// GetEducationalProgram return educational program by it`s id
+//
+// @Summary      Show educational program
+// @Description  get single educational program by ID
+// @Tags         educational program
+// @Accept       json
+// @Produce      json
+// @Param        id   path      string  true  "Educational program ID"
+// @Success      200  {object}  model.GetEducationalProgramResponse
+// @Failure      400
+// @Failure      404
+// @Failure      500
+// @Router       /api/v1/educationalProgram/{id} [get]
 func (h *Handler) GetEducationalProgram(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
 	id, err := uuid.FromString(params.ByName("id"))
 	if err != nil {
@@ -247,6 +341,19 @@ func (h *Handler) GetEducationalProgram(w http.ResponseWriter, r *http.Request, 
 	w.Write(respJSON)
 }
 
+// GetDiscipline return discipline by it`s id
+//
+// @Summary      Show discipline
+// @Description  get single discipline by ID
+// @Tags         discipline
+// @Accept       json
+// @Produce      json
+// @Param        id   path      string  true  "Discipline ID"
+// @Success      200  {object}  model.GetDisciplineResponse
+// @Failure      400
+// @Failure      404
+// @Failure      500
+// @Router       /api/v1/discipline/{id} [get]
 func (h *Handler) GetDiscipline(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
 	id, err := uuid.FromString(params.ByName("id"))
 	if err != nil {
@@ -276,6 +383,19 @@ func (h *Handler) GetDiscipline(w http.ResponseWriter, r *http.Request, params h
 	w.Write(respJSON)
 }
 
+// GetCourse return course by it`s id
+//
+// @Summary      Show course
+// @Description  get single course by ID
+// @Tags         course
+// @Accept       json
+// @Produce      json
+// @Param        id   path      string  true  "Course ID"
+// @Success      200  {object}  model.GetCourseResponse
+// @Failure      400
+// @Failure      404
+// @Failure      500
+// @Router       /api/v1/course/{id} [get]
 func (h *Handler) GetCourse(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
 	id, err := uuid.FromString(params.ByName("id"))
 	if err != nil {
@@ -305,6 +425,19 @@ func (h *Handler) GetCourse(w http.ResponseWriter, r *http.Request, params httpr
 	w.Write(respJSON)
 }
 
+// GetPortfolio return portfolio by it`s id
+//
+// @Summary      Show portfolio
+// @Description  get single portfolio by ID
+// @Tags         portfolio
+// @Accept       json
+// @Produce      json
+// @Param        id   path      string  true  "Portfolio ID"
+// @Success      200  {object}  model.GetPortfolioResponse
+// @Failure      400
+// @Failure      404
+// @Failure      500
+// @Router       /api/v1/portfolio/{id} [get]
 func (h *Handler) GetPortfolio(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
 	id, err := uuid.FromString(params.ByName("id"))
 	if err != nil {
@@ -334,6 +467,19 @@ func (h *Handler) GetPortfolio(w http.ResponseWriter, r *http.Request, params ht
 	w.Write(respJSON)
 }
 
+// GetStudent return student by it`s id
+//
+// @Summary      Show student
+// @Description  get single student by ID
+// @Tags         student
+// @Accept       json
+// @Produce      json
+// @Param        id   path      string  true  "Student ID"
+// @Success      200  {object}  model.GetStudentResponse
+// @Failure      400
+// @Failure      404
+// @Failure      500
+// @Router       /api/v1/student/{id} [get]
 func (h *Handler) GetStudent(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
 	id, err := uuid.FromString(params.ByName("id"))
 	if err != nil {
@@ -363,6 +509,19 @@ func (h *Handler) GetStudent(w http.ResponseWriter, r *http.Request, params http
 	w.Write(respJSON)
 }
 
+// GetTrajectory return trajectory by it`s id
+//
+// @Summary      Show trajectory
+// @Description  get single trajectory by ID
+// @Tags         trajectory
+// @Accept       json
+// @Produce      json
+// @Param        id   path      string  true  "Trajectory ID"
+// @Success      200  {object}  model.GetTrajectoryResponse
+// @Failure      400
+// @Failure      404
+// @Failure      500
+// @Router       /api/v1/trajectory/{id} [get]
 func (h *Handler) GetTrajectory(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
 	id, err := uuid.FromString(params.ByName("id"))
 	if err != nil {
@@ -392,31 +551,101 @@ func (h *Handler) GetTrajectory(w http.ResponseWriter, r *http.Request, params h
 	w.Write(respJSON)
 }
 
+// PostKnowledge return knowledge by it`s id
+//
+// @Summary      Post knowledge
+// @Description  post single knowledge (or check if it is already exists)
+// @Tags         knowledge
+// @Accept       json
+// @Produce      json
+// @Param        knowledgeTitle   body      string  true  "Knowledge title"
+// @Success      200  {object}  model.GetKnowledgeResponse
+// @Failure      400
+// @Failure      500
+// @Router       /api/v1/knowledge/ [post]
 func (h *Handler) PostKnowledge(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
 	defer r.Body.Close()
-	data, err := io.ReadAll(r.Body)
-	fmt.Println(string(data))
-	title := params.ByName("title")
-	_, err = h.App.PostKnowledge(title)
+
+	var req model.PostKnowledgeRequest
+	var unmarshalErr *json.UnmarshalTypeError
+
+	decoder := json.NewDecoder(r.Body)
+	decoder.DisallowUnknownFields()
+	err := decoder.Decode(&req)
+	if err != nil {
+		if errors.As(err, &unmarshalErr) {
+			slog.Error("Bad Request. Wrong Type provided for field "+unmarshalErr.Field, err)
+		} else {
+			slog.Error("Bad Request "+err.Error(), err)
+		}
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	resp, err := h.App.PostKnowledge(req.Title)
 	if err != nil {
 		slog.Error("error adding value to the knowledge table", err)
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
+
+	respJSON, err := json.Marshal(resp)
+	if err != nil {
+		slog.Error("error converting data to JSON format", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("content-Type", "application/json")
+	w.Write(respJSON)
 	w.WriteHeader(http.StatusOK)
 }
 
+// PostTechnology return technology by it`s id
+//
+// @Summary      Post technology
+// @Description  post single technology (or check if it is already exists)
+// @Tags         technology
+// @Accept       json
+// @Produce      json
+// @Param        technologyTitle   body      string  true  "Technology title"
+// @Success      200  {object}  model.GetTechnologyResponse
+// @Failure      400
+// @Failure      500
+// @Router       /api/v1/technology/ [post]
 func (h *Handler) PostTechnology(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
 	defer r.Body.Close()
-	data, err := io.ReadAll(r.Body)
-	fmt.Println(data)
-	title := params.ByName("title")
-	_, err = h.App.PostTechnology(title)
+
+	var req model.PostTechnologyRequest
+	var unmarshalErr *json.UnmarshalTypeError
+
+	decoder := json.NewDecoder(r.Body)
+	decoder.DisallowUnknownFields()
+	err := decoder.Decode(&req)
+	if err != nil {
+		if errors.As(err, &unmarshalErr) {
+			slog.Error("Bad Request. Wrong Type provided for field "+unmarshalErr.Field, err)
+		} else {
+			slog.Error("Bad Request "+err.Error(), err)
+		}
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	resp, err := h.App.PostTechnology(req.Title)
 	if err != nil {
 		slog.Error("error adding value to the technology table", err)
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
+
+	respJSON, err := json.Marshal(resp)
+	if err != nil {
+		slog.Error("error converting data to JSON format", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("content-Type", "application/json")
+	w.Write(respJSON)
 	w.WriteHeader(http.StatusOK)
 }
 
